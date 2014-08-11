764d2ed76c341aca9be5db51d8b8671509a1ef0c
ridiculousfish <corydoras@ridiculousfish.com>
2014-08-11 12:51:03 -0700

    Rewrite __fish_complete_ssh.fish to use docopt

--- share/functions/__fish_complete_ssh.fish
+++ share/functions/__fish_complete_ssh.fish
@@ -1,61 +1,72 @@
 
 function __fish_complete_ssh -d "common completions for ssh commands" --argument command
 
-        complete -c $command -s 1 --description "Protocol version 1 only"
-        complete -c $command -s 2 --description "Protocol version 2 only"
-	complete -c $command -s 4 --description "IPv4 addresses only"
-	complete -c $command -s 6 --description "IPv6 addresses only"
-	complete -c $command -s C --description "Compress all data"
-	complete -xc $command -s c --description "Encryption algorithm" -a "blowfish 3des des"
-	complete -r -c $command -s F --description "Configuration file"
-	complete -r -c $command -s i --description "Identity file"
-	complete -x -c $command -s o --description "Options" -a "
-		AddressFamily
-		BatchMode
-		BindAddress
-		ChallengeResponseAuthentication
-		CheckHostIP
-		Cipher
-		Ciphers
-		Compression
-		CompressionLevel
-		ConnectionAttempts
-		ConnectTimeout
-		ControlMaster
-		ControlPath
-		GlobalKnownHostsFile
-		GSSAPIAuthentication
-		GSSAPIDelegateCredentials
-		Host
-		HostbasedAuthentication
-		HostKeyAlgorithms
-		HostKeyAlias
-		HostName
-		IdentityFile
-		IdentitiesOnly
-		LogLevel
-		MACs
-		NoHostAuthenticationForLocalhost
-		NumberOfPasswordPrompts
-		PasswordAuthentication
-		Port
-		PreferredAuthentications
-		Protocol
-		ProxyCommand
-		PubkeyAuthentication
-		RhostsRSAAuthentication
-		RSAAuthentication
-		SendEnv
-		ServerAliveInterval
-		ServerAliveCountMax
-		SmartcardDevice
-		StrictHostKeyChecking
-		TCPKeepAlive
-		UsePrivilegedPort
-		User
-		UserKnownHostsFile
-		VerifyHostKeyDNS
-	"
-	complete -c $command -s v --description "Verbose mode"
+    set -l SSH_OPTIONS "
+        AddressFamily
+        BatchMode
+        BindAddress
+        ChallengeResponseAuthentication
+        CheckHostIP
+        Cipher
+        Ciphers
+        Compression
+        CompressionLevel
+        ConnectionAttempts
+        ConnectTimeout
+        ControlMaster
+        ControlPath
+        GlobalKnownHostsFile
+        GSSAPIAuthentication
+        GSSAPIDelegateCredentials
+        Host
+        HostbasedAuthentication
+        HostKeyAlgorithms
+        HostKeyAlias
+        HostName
+        IdentityFile
+        IdentitiesOnly
+        LogLevel
+        MACs
+        NoHostAuthenticationForLocalhost
+        NumberOfPasswordPrompts
+        PasswordAuthentication
+        Port
+        PreferredAuthentications
+        Protocol
+        ProxyCommand
+        PubkeyAuthentication
+        RhostsRSAAuthentication
+        RSAAuthentication
+        SendEnv
+        ServerAliveInterval
+        ServerAliveCountMax
+        SmartcardDevice
+        StrictHostKeyChecking
+        TCPKeepAlive
+        UsePrivilegedPort
+        User
+        UserKnownHostsFile
+        VerifyHostKeyDNS
+    "
+
+    complete -c $command --signature "
+        Usage: ssh [options]
+
+        Options:
+            -1  Protocol version 1 only
+            -2  Protocol version 2 only
+            -4  IPv4 addresses only
+            -6  IPv6 addresses only
+            -C  Compress all data
+            -v  Verbose mode
+            -c <cipher_spec>    Encryption algorithm
+            -F <config_file>    Configuration file
+            -i <identity_file>  Identity file
+            -o <option>         Options
+
+        Conditions:
+            <cipher_spec>  blowfish 3des des
+            <option>       $SSH_OPTIONS
+    "
 end
 
