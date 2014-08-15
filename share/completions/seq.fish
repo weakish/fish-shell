3617b8ae9e84fd7903551ddbc21f8b5b4eb6bd24
ridiculousfish <corydoras@ridiculousfish.com>
2014-08-15 13:51:19 -0700

    Port seq completions to docopt

--- share/completions/seq.fish
+++ share/completions/seq.fish
@@ -1,6 +1,10 @@
-
-complete -c seq -s f -l format -d 'Use printf style floating-point FORMAT'
-complete -c seq -s s -l separator -d 'Use STRING to separate numbers'
-complete -c seq -s w -l equal-width -d 'Equalize width with leading zeroes'
-complete -c seq -l help -d 'Display this help'
-complete -c seq -l version -d 'Output version information'
+complete --signature '
+    Usage:   seq [options] [<first> [<incr>]] <last>
+    Options:
+        -f <format>  Use printf style floating-point format
+        -s <string>  Use string to separate numbers
+        -t <string>  Use string to terminate sequence of numbers
+        -w           Equalize width with leading zeroes
+        --help       Display help
+        --version    Output version information
+'
