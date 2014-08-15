5b150198d0d9c4afd6d51b7ea0aaf5db6cd5e92c
ridiculousfish <corydoras@ridiculousfish.com>
2014-08-15 13:43:59 -0700

    Finish porting ssh completion to docopt

--- share/completions/ssh.fish
+++ share/completions/ssh.fish
@@ -4,48 +4,44 @@
 
 __fish_complete_ssh ssh
 
-complete -x -c ssh -d Hostname -a "
 
-(__fish_print_hostnames)
-
-(
-	#Prepend any username specified in the completion to the hostname
-	echo (commandline -ct)|sed -ne 's/\(.*@\).*/\1/p'
-)(__fish_print_hostnames)
+complete --signature "
+    Usage: ssh [<user> | <hostname>] [<command_to_run>] [options]
+    Options: -a  Disables forwarding of the authentication agent
+             -A  Enables forwarding of the authentication agent
+             -b <bind_address>  Interface to transmit from
+             -e <escape_char>  Sets the escape character for sessions with a pty
+             -f  Go to background
+             -g  Allow remote host to connect to local forwarded ports
+             -I  Smartcard device
+             -k  Disable forwarding of Kerberos tickets
+             -l <user>  User
+             -m  MAC algorithm
+             -n  Prevent reading from stdin
+             -N  Do not execute remote command
+             -p  Port
+             -q  Quiet mode
+             -s  Subsystem
+             -t  Force pseudo-tty allocation
+             -T  Disable pseudo-tty allocation
+             -x  Disable X11 forwarding
+             -X  Enable X11 forwarding
+             -L  Locally forwarded ports
+             -R  Remotely forwarded ports
+             -D  Dynamic port forwarding
+    Conditions:
+      <hostname>
+            (__fish_print_hostnames)
+            (
+            #Prepend any username specified in the completion to the hostname
+            echo (commandline -ct)|sed -ne 's/\(.*@\).*/\1/p'
+            )(__fish_print_hostnames)
+      
+      <user>            (__fish_print_users | grep -v '^_')@
+      <command_to_run>  (__fish_complete_subcommand --fcs-skip=2)
+      <bind_address>    (cat /proc/net/arp ^/dev/null| sgrep -v '^IP'|cut -d ' ' -f 1 ^/dev/null)
+      <escape_char>     \^ none
 "
-
-complete -x -c ssh -d User -a "
-(__fish_print_users | grep -v '^_')@
-"
-complete -c ssh --description "Command to run" -x -a '(__fish_complete_subcommand --fcs-skip=2)'
-
-complete -c ssh -s a --description "Disables forwarding of the authentication agent"
-complete -c ssh -s A --description "Enables forwarding of the authentication agent"
-complete -x -c ssh -s b --description "Interface to transmit from" -a "
-(
-	cat /proc/net/arp ^/dev/null| sgrep -v '^IP'|cut -d ' ' -f 1 ^/dev/null
-)
-"
-
-complete -x -c ssh -s e --description "Escape character" -a "\^ none"
-complete -c ssh -s f --description "Go to background"
-complete -c ssh -s g --description "Allow remote host to connect to local forwarded ports"
-complete -c ssh -s I --description "Smartcard device"
-complete -c ssh -s k --description "Disable forwarding of Kerberos tickets"
-complete -c ssh -s l -x -a "(__fish_complete_users)" --description "User"
-complete -c ssh -s m --description "MAC algorithm"
-complete -c ssh -s n --description "Prevent reading from stdin"
-complete -c ssh -s N --description "Do not execute remote command"
-complete -c ssh -s p -x --description "Port"
-complete -c ssh -s q --description "Quiet mode"
-complete -c ssh -s s --description "Subsystem"
-complete -c ssh -s t --description "Force pseudo-tty allocation"
-complete -c ssh -s T --description "Disable pseudo-tty allocation"
-complete -c ssh -s x --description "Disable X11 forwarding"
-complete -c ssh -s X --description "Enable X11 forwarding"
-complete -c ssh -s L --description "Locally forwarded ports"
-complete -c ssh -s R --description "Remotely forwarded ports"
-complete -c ssh -s D --description "Dynamic port forwarding"
 
 # Since ssh runs subcommands, it can accept any switches
 complete -c ssh -u
